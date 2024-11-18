52cb1983714b6fd9bac5c511ed4220ac1c6d3926
My Name <myEmail@example.com>
2024-11-18 18:06:54 +0800

    Second step:Create function ComparisonOfInitialNumbers

--- src/MultiplicationTableBuilder.java
+++ src/MultiplicationTableBuilder.java
@@ -5,11 +5,19 @@ public class MultiplicationTableBuilder {
         int start = 2;
         int end = 4;
         String multiplicationTable = builder.build(start, end);
-
         System.out.println(multiplicationTable);
     }
 
     public String build(int start, int end) {
         return "";
     }
+    public boolean ComparisonOfInitialNumbers(int start,int end){
+        boolean isTrueInitialComparison=false;
+        if(start<=end){
+            isTrueInitialComparison=true;
+            return isTrueInitialComparison;
+        }
+        return isTrueInitialComparison;
+    }
+
 }
