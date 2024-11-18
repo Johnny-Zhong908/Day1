108bd41007e07720c1d4fa35ed9389aff2493784
My Name <myEmail@example.com>
2024-11-18 17:42:36 +0800

    First step:Create function main

--- src/MultiplicationTableBuilder.java
+++ src/MultiplicationTableBuilder.java
@@ -12,14 +12,7 @@ public class MultiplicationTableBuilder {
     public String GenerateMultiplicationTable(int start, int end) {
         return "";
     }
-    public boolean ComparisonOfInitialNumbers(int start,int end){
-        boolean isTrueInitialComparison=false;
-        if(start<=end){
-            isTrueInitialComparison=true;
-            return isTrueInitialComparison;
-        }
-        return isTrueInitialComparison;
-    }
+
 
 
 
